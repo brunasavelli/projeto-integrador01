d67f7825717ee227f69721d5cc9a9b752adc0b90
VINICIUS DA SILVA BUENO <vinicius.sb8@puccampinas.edu.br>
2026-05-08 21:02:37 -0300

    conexao pronta, estruturando agora

--- src/config/database.py
+++ src/config/database.py
@@ -2,15 +2,37 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(override=True)
 
-def conectar():
 
-    conexao = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
+def obter_conexao():
 
-    return conexao
+    conexao = None
+    try:
+        conexao = mysql.connector.connect(
+            host=os.getenv("HOST"),
+            user=os.getenv("USER"),
+            password=os.getenv("PASSWORD"),
+            database=os.getenv("NAME")
+        )
+
+        print("Banco conectado com sucesso!")
+        return conexao
+
+    except Exception as erro:
+        print("Erro ao conectar:")
+        print(erro)
+
+
+def fechar_conexao():
+
+    try:
+        conexao.close()
+        conexao = None
+        print("Conexão encerrada com sucesso!")
+    
+    except Exception as erro:
+        print("Erro ao fechar conexão:")
+        print(erro)
+
+
