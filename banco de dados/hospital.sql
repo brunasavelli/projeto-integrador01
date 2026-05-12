ec3a9845521095251edc7e960c14f1d20eaf84fd
FelipeV21 <felipe.verissimooliveira21@gmail.com>
2026-05-12 12:17:19 -0300

    só mexi numa virgula que estava errada

--- banco de dados/hospital.sql
+++ banco de dados/hospital.sql
@@ -4,7 +4,7 @@ USE hospital_db;
 CREATE TABLE medicos (
     id_medico INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    especialidade VARCHAR(100) NOT NULL,
+    especialidade VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE pacientes (
