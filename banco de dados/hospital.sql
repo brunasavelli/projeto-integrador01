b37dec4d239a7ab35b1a6b37e40c951c922e6c5d
FelipeV21 <felipe.verissimooliveira21@gmail.com>
2026-05-13 21:41:30 -0300

    Update hospital.sql

--- banco de dados/hospital.sql
+++ banco de dados/hospital.sql
@@ -4,7 +4,7 @@ USE hospital_db;
 CREATE TABLE medicos (
     id_medico INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    especialidade VARCHAR(100) NOT NULL,
+    especialidade VARCHAR(100) NOT NULL #Estava com virgula a mais
 );
 
 CREATE TABLE pacientes (
