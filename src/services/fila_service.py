f321d1520e227f4e7c93e0dbd1432906a6b88676
Kell <raqueljacominib@gmail.com>
2026-05-27 19:48:17 -0300

    colocando linha de comentario na função adicionar paciente explicando o que cada parte do código faz.

--- src/services/fila_service.py
+++ src/services/fila_service.py
@@ -7,30 +7,43 @@ def calcular_media(impacto, urgencia):
 
 
 def adicionar_paciente():
+    #tenta abrir a conexão com o banco de dados
     conexao = obter_conexao()
 
+    #se não conseguir conectar para a função
     if conexao is None:
         return None
 
     try:
+        #cria um "cursor" que é o objeto usado para executar comandos sql
         cursor = conexao.cursor()
 
+        #comando sql para inserir o paciente da tabela
         sql = """
         INSERT INTO pacientes (nome, data_nascimento, telefone, email)
         VALUES ('ana', '19-05-2008', '(19)98855-2233', 'ana@gmail.com')
         """
 
+        #executa o comando sql no banco
         cursor.execute(sql)
+
+        #salva (confirma) a altração no banco
         conexao.commit()
 
-        return cursor.lastrowid  # retorna o ID inserido
+        #retorna o ID do ultimo registro inserido 
+        return cursor.lastrowid  
 
     except Exception as erro:
+        #se encontrar algum erro, mostra na tela
         print("Erro:", erro)
 
+        #fecha o cursor
         cursor.close()
+
+        #fecha a conexao com o banco
         fechar_conexao(conexao)
 
+        #retorna none indicando que deu erro
         return None
     
        
