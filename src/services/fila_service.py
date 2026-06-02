28b721b05cac4bae597601c8b8a1c57bbd7b99de
Kell <raqueljacominib@gmail.com>
2026-06-01 21:57:12 -0300

    update fila_service.py
    Removed unused functions for calculating average and managing patient queue.

--- src/services/fila_service.py
+++ src/services/fila_service.py
@@ -1,11 +1,3 @@
-from config.database import fechar_conexao, obter_conexao
-from data.fila import fila
-from utils.input_utils import ler_inteiro_entre
-
-def calcular_media(impacto, urgencia):
-    return (impacto + urgencia) / 2
-
-
 def adicionar_paciente():
     #tenta abrir a conexão com o banco de dados
     conexao = obter_conexao()
@@ -45,60 +37,3 @@ def adicionar_paciente():
 
         #retorna none indicando que deu erro
         return None
-    
-       
-
-
-def chamar_proximo():
-    if not fila:
-        print("ℹ️ Não há pacientes na fila.\n")
-        return
-
-    indice_maior = 0
-
-    for i in range(1, len(fila)):
-
-        if fila[i][3] > fila[indice_maior][3]:
-            indice_maior = i
-
-        elif fila[i][3] == fila[indice_maior][3]:
-
-            if fila[i][2] > fila[indice_maior][2]:
-                indice_maior = i
-
-    paciente = fila.pop(indice_maior)
-
-    nome, impacto, urgencia, media = paciente
-
-    print("\nChamando próximo paciente:\n")
-    print(f"Nome: {nome}")
-    print(f"Impacto: {impacto}")
-    print(f"Urgência: {urgencia}")
-    print(f"Média: {media:.1f}\n")
-
-
-def ver_fila():
-    if not fila:
-        print("Fila vazia.\n")
-        return
-
-    print("\n--- FILA ATUAL ---")
-
-    for nome, impacto, urgencia, media in fila:
-        print(
-            f"{nome} | "
-            f"Urgência: {urgencia} | "
-            f"Impacto: {impacto} | "
-            f"Média: {media:.1f}"
-        )
-
-    print("------------------\n")
-
-
-def mostrar_menu():
-    print("===== SISTEMA HOSPITALAR =====")
-    print("1 - Adicionar paciente")
-    print("2 - Chamar próximo paciente")
-    print("3 - Ver fila")
-    print("0 - Sair")
-    print("==============================\n")
